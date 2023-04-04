2374ff0719fe78fcd9cc863c5046adc95904afa9
heku66 <102395422+heku66@users.noreply.github.com>
2023-04-04 18:08:05 +0800

    Update entrypoint.sh

--- samba-server/rootfs/usr/bin/entrypoint.sh
+++ samba-server/rootfs/usr/bin/entrypoint.sh
@@ -6,6 +6,6 @@ set -e
 
 [[ "$DEBUG" == "true" ]] && set -x
 
-echo -e "${PASSWORD:-$(hostname)}\n${PASSWORD:-$(hostname)}" | smbpasswd -a -s -c /etc/samba/smb.conf admin
+echo -e "${PASSWORD:-$(hostname)}\n${PASSWORD:-$(hostname)}" | smbpasswd -a -s -c /etc/samba/smb.conf root
 
 exec "$@"
